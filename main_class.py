cb4ba8c43046dca3f620e0371b47160954bbd612
Samuel <samuel.mf1998@gmail.com>
2023-12-06 21:09:03 +0000

    añadiendo info extra

--- main_class.py
+++ main_class.py
@@ -1,24 +1,34 @@
-from fastapi import FastAPI, Body
-from fastapi.responses import HTMLResponse
-from pydantic import BaseModel
-from typing import Optional
+from fastapi import FastAPI, Body, Path, Query
+#Query es como Path pero para longitud de str
+from fastapi.responses import HTMLResponse,JSONResponse
+from pydantic import BaseModel, Field
+from typing import Optional, List
+from jwt_config import dame_token
 
+#No veo cambios en la respuesta de la API al añadir List
 
 app = FastAPI()
 app.title = "Aplicacion de ventas"
 app.version = "1.0.1"
+
 ventas = [
     {"id": 1, "fecha": "01/12/2023", "tienda": "Tienda01", "importe": 100},
     {"id": 2, "fecha": "10/12/2023", "tienda": "Tienda02", "importe": 355},
     {"id": 3, "fecha": "10/12/2023", "tienda": "Tienda03", "importe": 355},
 ]
 
+class Usuario(BaseModel):
+    email:str
+    clave:str
 
 class Sales(BaseModel):  # creamos un modelo
-    id: Optional[int]
+    id: int =Field(ge=0,le=20) #mayor que cero y menor que 20
+    #id: Optional[int]
     fecha: str
-    tienda: str
+    tienda: str=Field(min_length=4,max_length=10)
+    #tienda: str
     importe: float
+    
 
 
 # crear punto de entrada o endpoint
@@ -34,22 +44,24 @@ def mensaje():
 
 
 # direccion/ventas
-@app.get("/ventas", tags=["Ventas"])
-def dame_ventas():
-    return ventas
+@app.get("/ventas", tags=["Ventas"],response_model=List[Sales])
+def dame_ventas()->List[Sales]: #No veo cambios el añadir List
+    return JSONResponse(content=ventas) #para devolver un JSON
 
 
 # direccion/ventas/numero_personalizado
 # Si el numero existe en el id de ventas me devuelve el diccionario entero,
 # sino me devuelve una lista vacia
-@app.get("/ventas/{id}", tags=["Ventas"])
-def dame_ventas(id: int):
+@app.get("/ventas/{id}", tags=["Ventas"],response_model=List[Sales],status_code=200)
+def dame_ventas(id: int=Path(ge=1,le=10)) -> Sales: #el ge y el le es para poner min y max permitidos
     for elem in ventas:
-        if elem["id"] == id:
-            return elem
-    return "El valor introducido no existe"
+        if elem['id'] == id:
+            return JSONResponse(content=elem,status_code=200)
+    message = {"mensaje": f"El elemento con id {id} ya existe"}    
+    return JSONResponse(content=message, status_code=404)
 
 
+    
 # @app.get("/ventas", tags=["Ventas"])  # esta sobre escribiria la de arriba
 # def dame_ventas_por_tienda(tienda: str):
 #     return tienda
@@ -73,29 +85,32 @@ def dame_ventas_por_tienda(tienda: str, id: int):
 # post
 
 
-@app.post("/ventas", tags=["Ventas"])
+@app.post("/ventas", tags=["Ventas"],status_code=201)
 def crea_ventas(venta: Sales):
     for sale in ventas:
-        if sale.id == venta.id:
-            return f"el elemento con id {venta.id} ya existe"
-
-    ventas.append(venta)
-
-    return ventas
+        if sale['id'] == venta.id:
+            return 'Ya existe un elemento con este id'
+    ventas.append({"id": venta.id, "fecha": venta.fecha, "tienda": venta.tienda, "importe": venta.importe})
+    return JSONResponse(content={'mensaje':'Venta registrada',
+                                 'content':ventas},status_code=200)
 
 
 # put
 # modificar datos
 
 
-@app.put("/ventas/{id}", tags=["Ventas"])
+@app.put("/ventas/{id}", tags=["Ventas"],status_code=201)
 def actualiza_ventas(id: int, venta: Sales):
     for elem in ventas:
-        if elem.id == id:
-            elem.fecha = venta.fecha
-            elem.tienda = venta.tienda
-            elem.importe = venta.importe
-    return ventas
+        if elem['id'] == id:
+            elem['id']=venta.id
+            elem['fecha'] = venta.fecha
+            elem['tienda'] = venta.tienda
+            elem['importe'] = venta.importe
+    return JSONResponse(content={'mensaje':'Venta actualizada',
+                                 'content':ventas},status_code=200)
+
+
 
 
 # delete
@@ -104,9 +119,20 @@ def actualiza_ventas(id: int, venta: Sales):
 @app.delete("/ventas{id}", tags=["Ventas"])
 def borra_ventas(id: int):
     for elem in ventas:
-        if elem.id == id:
+        if elem['id'] == id:
             ventas.remove(elem)
-    return ventas
+    return JSONResponse(content={'mensaje':'Venta eliminada',
+                                 'content':ventas})
+        
+             
 
 
-# models
+#creamos ruta para login
+
+@app.post('/login',tags=['Autenticacion'])
+def login(usuario:Usuario):
+    if usuario.email == 'samuel.mf1998@gmail.com' and usuario.clave == '1234':
+        token:str=dame_token(usuario.dict())
+        return JSONResponse(status_code=200,content=token)
+    message='Error de autenticacion'
+    return JSONResponse(content=message,status_code=401)
